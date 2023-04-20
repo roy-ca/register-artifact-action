fc07aedc46a5f4cc0cf7b34be6f2bd906c7dac6e
roy-ca <roy.ca@servicenow.com>
2023-04-20 13:22:53 +0530

    Updated

--- src/main.js
+++ src/main.js
@@ -1,49 +1,75 @@
 const core = require('@actions/core');
 const axios = require('axios');
-//const { Octokit } = require("@octokit/rest");
 
-// const octokit = new Octokit({
-//     auth: "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"
-//   });
 
 (async function main() {
-
-    //let instanceUrl = core.getInput('instance-url', { required: true });
-    let instanceUrl = 'https://api.github.com/';
+    let instanceUrl = core.getInput('instance-url', { required: true });
+    const toolId = core.getInput('tool-id', { required: true });
+    const username = core.getInput('devops-integration-user-name', { required: false });
+    const password = core.getInput('devops-integration-user-password', { required: false });
     const securityToken = core.getInput('devops-security-token', { required: false});
-    //const toolId = core.getInput('tool-id', { required: true });
-    let toolId = 'repos/roy-ca/MyGithubActions/hooks/409489218';
-    let snowResponse = {};
-    try {
-        let endpoint = `${instanceUrl}${toolId}`;
-        console.log("Endpoint:"+endpoint);
-        const defaultHeaders = {
-            'Accept': 'application/json',
-            'Authorization': 'Bearer ' + `${securityToken}`
-         };
-         let httpHeaders = { headers: defaultHeaders };
-         snowResponse = await axios.get(endpoint, httpHeaders);
-         console.log("Response:"+JSON.stringify(snowResponse));
-    } catch(e) {
-            // Response received, but with an error status code
-            console.log("Error:"+JSON.stringify(e));
+    const jobName = core.getInput('job-name', { required: true });
 
+    let artifacts = core.getInput('artifacts', { required: true });
+    
+    try {
+        artifacts = JSON.parse(artifacts);
+    } catch (e) {
+        core.setFailed(`Failed parsing artifacts ${e}`);
+        return;
     }
 
-    // try {
-    //     // Call the GitHub API to get webhook details
-    //     const response = await octokit.repos.getWebhook({
-    //       owner: 'roy-ca',
-    //       repo: 'MyGithubActions',
-    //       webhook_id: '409489218'
-    //     });
+    let githubContext = core.getInput('context-github', { required: true });
+
+    try {
+        githubContext = JSON.parse(githubContext);
+    } catch (e) {
+        core.setFailed(`Exception parsing github context ${e}`);
+    }
+
+    let payload;
     
-    //     // Extract and return webhook details from the response
-    //    console.log("Response:"+JSON.stringify(response));
-    //   } catch (error) {
-    //     console.error("Failed to get webhook details:", error);
-    //     throw error;
-    //   }
-    })();
+    try {
+        instanceUrl = instanceUrl.trim();
+        if (instanceUrl.endsWith('/'))
+            instanceUrl = instanceUrl.slice(0, -1);
 
+        payload = {
+            'artifacts': artifacts,
+            'pipelineName': `${githubContext.repository}/${githubContext.workflow}`,
+            'stageName': jobName,
+            'taskExecutionNumber': `${githubContext.run_id}` + '/attempts/' + `${githubContext.run_attempt}`, 
+            'branchName': `${githubContext.ref_name}`
+        };
+        console.log("paylaod to register artifact: " + JSON.stringify(payload));
+    } catch (e) {
+        core.setFailed(`Exception setting the payload to register artifact ${e}`);
+        return;
+    }
 
+    let snowResponse;
+    const endpoint = `${instanceUrl}/api/sn_devops/devops/artifact/registration?orchestrationToolId=${toolId}`;
+
+    try {
+        const token = `${username}:${password}`;
+        const encodedToken = Buffer.from(token).toString('base64');
+
+        const defaultHeaders = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            'Authorization': 'Basic ' + `${encodedToken}`
+        };
+
+        let httpHeaders = { headers: defaultHeaders };
+        snowResponse = await axios.post(endpoint, JSON.stringify(payload), httpHeaders);
+    } catch (e) {
+        if (e.message.includes('ECONNREFUSED') || e.message.includes('ENOTFOUND') || e.message.includes('405')) {
+            core.setFailed('ServiceNow Instance URL is NOT valid. Please correct the URL and try again.');
+        } else if (e.message.includes('401')) {
+            core.setFailed('Invalid Credentials. Please correct the credentials and try again.');
+        } else {
+            core.setFailed('ServiceNow Artifact Versions are NOT created. Please check ServiceNow logs for more details.');
+        }
+    }
+    
+})();
